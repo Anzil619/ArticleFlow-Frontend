42bb0ebe4f42ded67d6a30754ec35bc4ccbb32c5
Anzil619 <anzilaz24@gmail.com>
2023-12-12 17:34:25 +0530

    changed the word truncate function

--- src/components/Cards/ArticleCards.jsx
+++ src/components/Cards/ArticleCards.jsx
@@ -120,7 +120,7 @@ export function ArticleCards({
     return description;
   };
 
-  const truncatedDescription = truncateDescription(description, 35);
+  const truncatedDescription = truncateDescription(description, 20);
 
   const [isDisliked, setIsDisliked] = useState(false);
   const handledislike = async () => {
